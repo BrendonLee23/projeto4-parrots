d60fa000bb374dd9682d96bd19bb49c703d5768b
Brendo Moreira <104690858+BrendonLee23@users.noreply.github.com>
2023-04-08 22:54:01 -0300

    Efeito de virar cartas Aplicado

---
+++ Assets/JS/script.js
@@ -0,0 +1,18 @@
+let result = ''
+while (true) {
+    result = prompt("Com quantas cartas você quer jogar?")
+
+    if (result % 2 === 0 && (result >= 4 && result <= 14)) {
+        break
+    }
+}
+
+
+function virarCarta(carta) {
+    const carta1 = carta.querySelector(".carta1");
+    carta1.classList.toggle("front");
+
+    const carta2 = carta.querySelector(".carta2");
+    carta2.classList.toggle("back");
+}
+
