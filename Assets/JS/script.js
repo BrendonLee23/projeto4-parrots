a764c26eaed906196dba0a56429271c156d8c870
Brendo Moreira <104690858+BrendonLee23@users.noreply.github.com>
2023-04-09 14:56:24 -0300

    Quantidade de cartas selecionadas = cartas apresentadas ativo

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,11 +1,42 @@
-let result = ''
-while (true) {
-    result = prompt("Com quantas cartas você quer jogar?")
+(function shuffle(){
+    embaralharCartas()
+})()
 
-    if (result % 2 === 0 && (result >= 4 && result <= 14)) {
-        break
+function embaralharCartas() {
+    // pegar a quantidade de cartas escolhida pelo usuário
+    let result = ''
+    while (true) {
+        result = prompt("Com quantas cartas você quer jogar?")
+
+        if (result % 2 === 0 && (result >= 4 && result <= 14)) {
+            break
+        }
     }
+
+    // criar uma lista com a quantidade de cartas que o usuario escolheu e remover oa classe d-none
+    const cartas = document.querySelectorAll(".d-none")
+    let cartasSelecionadas = []
+    
+
+    for (let i = 0; i < Number(result); i++) {
+        cartasSelecionadas.push(cartas[i])
+    }
+    console.log(cartasSelecionadas)
+
+    // embaralhar todas elas
+    cartasSelecionadas.forEach((carta => {
+        let ramdomPos = Math.floor(Math.random() * 14)
+        carta.style.order = ramdomPos
+        carta.classList.remove("d-none")
+    }))
 }
+
+
+
+
+
+
+
 
 
 function virarCarta(carta) {
@@ -16,5 +47,5 @@ function virarCarta(carta) {
     carta2.classList.toggle("back");
 }
 
-//  nem mensage
+
 
